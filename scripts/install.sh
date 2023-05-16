d0edfacab930954d0457552c6a5852d7c17f1a34
lindelanimcebo <lindelanimcebo@gmail.com>
2023-05-16 13:46:35 +0200

    dependency installation script

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd
+git clone https://github.com/libuvc/libuvc
+cd libuvc
+mkdir build
+cd build
+cmake ..
+make && sudo make install
+
+sudo echo 'sudo sh -c "echo 'SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"1e4e\", ATTRS{idProduct}==\"0100\", SYMLINK+=\"pt1\", GROUP=\"usb\", MODE=\"666\"' > /etc/udev/rules.d/99-pt1.rules"' >> ~/.bashrc
+
